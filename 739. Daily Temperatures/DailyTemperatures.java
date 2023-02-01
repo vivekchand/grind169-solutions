042165031f8040de5ff793ce24c781024231d813
Vivek Chand <vivekchand19@gmail.com>
2023-02-02 00:25:58 +0100

    Daily Temperatures

---
+++ 739. Daily Temperatures/DailyTemperatures.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int currDay = 0; currDay < n; currDay++) {
+            int currentTemp = temperatures[currDay];
+            // Pop until the current day's temperature is not
+            // warmer than the temperature at the top of the stack
+            while (!stack.isEmpty() && temperatures[stack.peek()] < currentTemp) {
+                int prevDay = stack.pop();
+                answer[prevDay] = currDay - prevDay;
+            }
+            stack.push(currDay);
+        }
+        
+        return answer;
+    }
+}
