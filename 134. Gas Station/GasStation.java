1cb5c7256ca52fa6d293ff6d91a847b5d6e6f256
Vivek Chand <vivekchand19@gmail.com>
2023-02-03 00:27:51 +0100

    Update solution

--- 134. Gas Station/GasStation.java
+++ 134. Gas Station/GasStation.java
@@ -1,14 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int start = 0, total = 0, tank = 0;
+        if(IntStream.of(gas).sum() < IntStream.of(cost).sum()) return -1;
+        int start = 0, tank = 0;
         for (int i = 0; i < gas.length; i++) {
             tank += gas[i] - cost[i];
             if (tank < 0) {
                 start = i + 1;
-                total += tank;
                 tank = 0;
             }
         }
-        return (total + tank < 0) ? -1 : start;
+        return start;
     }
 }
