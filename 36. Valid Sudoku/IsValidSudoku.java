6820ea5c5b0b86d029c304fa2cbac5566589e834
Vivek Chand <vivekchand19@gmail.com>
2023-02-04 14:29:19 +0100

    Valid Sudoku

---
+++ 36. Valid Sudoku/IsValidSudoku.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            HashSet<Character> rows = new HashSet<>();
+            HashSet<Character> columns = new HashSet<>();
+            HashSet<Character> cube = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.' && !rows.add(board[i][j]))
+                    return false;
+                if (board[j][i] != '.' && !columns.add(board[j][i]))
+                    return false;
+                int RowIndex = 3 * (i / 3);
+                int ColIndex = 3 * (i % 3);
+                if (board[RowIndex + j / 3][ColIndex + j % 3] != '.' && !cube.add(board[RowIndex + j / 3][ColIndex + j % 3]))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
