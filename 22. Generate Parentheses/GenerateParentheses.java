f66135a4875cd28191f982d6c856f90a74ce6a0f
Vivek Chand <vivekchand19@gmail.com>
2023-03-02 22:53:03 +0100

    Generate Parentheses

---
+++ 22. Generate Parentheses/GenerateParentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    
+    public void backtrack(List<String> result, String current, int open, int close, int n) {
+        if (current.length() == n * 2) {
+            result.add(current);
+            return;
+        }
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+}
