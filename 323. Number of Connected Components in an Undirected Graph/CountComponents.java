ba702e5c26013defacb8d5e0630c6c2298e7992c
Vivek Chand <vivekchand19@gmail.com>
2023-03-04 22:56:49 +0100

    Count Components

---
+++ 323. Number of Connected Components in an Undirected Graph/CountComponents.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+     private void dfs(List<Integer>[] adjList, int[] visited, int startNode) {
+        visited[startNode] = 1;
+         
+        for (int i = 0; i < adjList[startNode].size(); i++) {
+            if (visited[adjList[startNode].get(i)] == 0) {
+                dfs(adjList, visited, adjList[startNode].get(i));
+            }
+        }
+    }
+    
+    public int countComponents(int n, int[][] edges) {
+        int components = 0;
+        int[] visited = new int[n];
+        
+        List<Integer>[] adjList = new ArrayList[n]; 
+        for (int i = 0; i < n; i++) {
+            adjList[i] = new ArrayList<Integer>();
+        }
+        
+        for (int i = 0; i < edges.length; i++) {
+            adjList[edges[i][0]].add(edges[i][1]);
+            adjList[edges[i][1]].add(edges[i][0]);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                components++;
+                dfs(adjList, visited, i);
+            }
+        }
+        return components;
+    }
+}
