175ccfcd46b1b25e9d73556f6226ecf1096c7c35
Vivek Chand <vivekchand19@gmail.com>
2023-03-08 00:59:59 +0100

    cleanup

--- 560. Subarray Sum Equals K/SubarraySum.java
+++ 560. Subarray Sum Equals K/SubarraySum.java
@@ -1,7 +1,7 @@
 public class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0, sum = 0;
-        HashMap < Integer, Integer > map = new HashMap < > ();
+        HashMap<Integer, Integer> map = new HashMap <> ();
         map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
