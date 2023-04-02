19a2e6daace70b2c38e4f528924be71d44a64037
Vivek Chand <vivekchand19@gmail.com>
2023-04-02 12:09:55 +0200

    Update IsValidBST.java

--- 98. Validate Binary Search Tree/IsValidBST.java
+++ 98. Validate Binary Search Tree/IsValidBST.java
@@ -3,12 +3,13 @@ class Solution {
 		return check(root, null, null);
 	}
  
-    private boolean check(TreeNode root, Integer minVal, Integer maxVal) {
+	
+	private boolean check(TreeNode root, Integer minVal, Integer maxVal) {
 		if(root == null) return true;
 
 		if(minVal != null && root.val <= minVal || maxVal != null && root.val >= maxVal) {
 			return false;
 		}
 		return check(root.left, minVal, root.val) && check(root.right, root.val, maxVal);
-    } 
+	} 
 }
