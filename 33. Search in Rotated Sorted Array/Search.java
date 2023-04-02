0d1fe42d3e5bff71091a1c8bee9d725b970acbd5
Vivek Chand <vivekchand19@gmail.com>
2023-04-02 12:17:14 +0200

    Update Search.java

--- 33. Search in Rotated Sorted Array/Search.java
+++ 33. Search in Rotated Sorted Array/Search.java
@@ -1,8 +1,8 @@
 class Solution {
-	public int search(int[] nums, int target) {
-		int low = 0;
+   public int search(int[] nums, int target) {
+        int low = 0;
         int high = nums.length-1;
-        
+
         while(low <= high) {
             int mid = (low + high) / 2;
             if(nums[mid] == target) return mid;
