58b4cd209bbf4bba4121d20cdf093f4da81a5329
Vivek Chand <vivekchand19@gmail.com>
2024-10-20 17:21:27 +0200

    Update SurroundedRegions.java

---
+++ src/main/java/com/leetcode/SurroundedRegions.java
@@ -0,0 +1,44 @@
+package com.leetcode;
+
+class SurroundedRegions {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        // Check the border cells
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+        
+        // Convert remaining 'O's to 'X's and '*'s back to 'O's
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '*') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        
+        board[i][j] = '*';
+        
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
