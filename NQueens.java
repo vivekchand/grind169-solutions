cff40db3ab1bffc2aae486f46e2118487b39a4b6
Vivek Chand <vivekchand19@gmail.com>
2024-10-20 18:15:05 +0200

    Update NQueens.java

---
+++ NQueens.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueens {
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        char[][] board = new char[n][n];
+        
+        // Initialize the board with empty squares
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        
+        backtrack(board, 0, solutions);
+        return solutions;
+    }
+    
+    private static void backtrack(char[][] board, int row, List<List<String>> solutions) {
+        if (row == board.length) {
+            solutions.add(constructSolution(board));
+            return;
+        }
+        
+        for (int col = 0; col < board.length; col++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(board, row + 1, solutions);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    private static boolean isValid(char[][] board, int row, int col) {
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private static List<String> constructSolution(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (char[] row : board) {
+            solution.add(new String(row));
+        }
+        return solution;
+    }
+    
+    public static void main(String[] args) {
+        int n = 4; // Change this to solve for different board sizes
+        List<List<String>> solutions = solveNQueens(n);
+        
+        System.out.println("Number of solutions for " + n + "-Queens: " + solutions.size());
+        for (int i = 0; i < solutions.size(); i++) {
+            System.out.println("Solution " + (i + 1) + ":");
+            for (String row : solutions.get(i)) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+}
